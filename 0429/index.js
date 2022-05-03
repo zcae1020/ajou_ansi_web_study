04604699116383449f481d83b75bc9d087b09a78
zcae1020 <zcae1020@naver.com>
2022-05-03 11:56:15 +0900

    express-session

--- 0429/index.js
+++ 0429/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const session = require('express-session')
+const fileStore = require('session-file-store')
+
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(session({
+  secret: 'keyboard cat',
+}))
+
 app.get('/', (req, res) => {
+  console.log(req.sessionID)
   res.send('Welcome to Section')
 })
 

4567fc62798f107b902b72b36e80d897461f2103
zcae1020 <zcae1020@naver.com>
2022-05-04 16:50:16 +0900

    get value by session

--- 0429/index.js
+++ 0429/index.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const session = require('express-session')
-const fileStore = require('session-file-store')
+const bodyParser = require('body-parser');
+const fs = require('fs')
 
 const app = express()
-const port = process.env.PORT || 8080
 
-app.use(session({
-  secret: 'keyboard cat',
-}))
+app.use(session({secret: 'keyboard cat'}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  console.log(req.sessionID)
-  res.send('Welcome to Section')
-})
+  fs.exists(__dirname+`/data/${req.sessionID}.txt`, (e) => {
+    if(e){
+      fs.readFile(__dirname+`/data/${req.sessionID}.txt`, (err, data) => {
+        if (err) throw err;
+        res.sendFile(__dirname+`/data/${req.sessionID}.txt`)
+      });
+    }
+    else{
+      res.send(
+        `<form method="POST" action="/">
+          <input type="text" name="data">
+          <input type="submit">
+        </form>`)
+    }
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/', function (req, res) {
+  fs.writeFile(__dirname+`/data/${req.sessionID}.txt`, req.body.data, (e) => {
+    if (e) throw e;
+    console.log('The file has been saved!');
+  });
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log("app listening at http://localhost:3000")
 })
